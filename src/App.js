2461770deb8937c35cb719f1cb540f0bcdaa5318
Jeffrey Lu <146838940+JeffreyLuW@users.noreply.github.com>
2023-12-11 15:13:05 +0800

    handle-routes-shop-and-cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {NavBar} from './components/navbar.jsx';
+import {Shop} from './pages/shop/shop';
+import {Cart} from './pages/cart/cart';
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+        <Router>
+            <NavBar></NavBar>
+            <Routes>
+                <Route path="/"  element={<Shop />}/>
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </Router>
+    </div>
   );
 }
 
