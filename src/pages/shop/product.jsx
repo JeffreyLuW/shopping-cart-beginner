aeb9b93a05a1b58808ef0f7ee9f6961d1225f2e7
Jeffrey Lu <146838940+JeffreyLuW@users.noreply.github.com>
2023-12-12 15:29:27 +0800

    display-picture-and-add-shopcss

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from "react";
+// 
 
 export const Product = (props) => {
-  const {id,productName,price,productImage} = props.data
+  const { id, productName, price, productImage } = props.data;
   return (
-    <div>
-        <ul>
-            <li>{id}</li>
-            <li>{productName}</li>
-            <li>{price}</li>
-            <li>{productImage}</li>
-        </ul>
+    <div className="product">
+      <img src={productImage} alt=''/>
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> ${price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
